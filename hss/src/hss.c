57770a28dd269043b9b8b633ca40b75c78000767
Gustavo Banegas <gustavo@cryptme.in>
2022-03-02 10:38:35 +0100

    initial

---
+++ hss/src/hss.c
@@ -0,0 +1,8 @@
+/*
+ * hss.c
+ *
+ *  Created on: Mar 2, 2022
+ *      Author: Gustavo Banegas
+ */
+
+
---
+++ hss/src/hss.h
@@ -0,0 +1,13 @@
+/*
+ * hss.h
+ *
+ *  Created on: Mar 2, 2022
+ *      Author: Gustavo Banegas
+ */
+
+#ifndef HSS_H_
+#define HSS_H_
+
+
+
+#endif /* HSS_H_ */
